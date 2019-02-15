3b38b97122b584e21a4b36326e79a7f160656769
Ayoola FALOLA <ayoola@ayoo.la>
2019-02-15 13:45:58 +0100

    Uninstall previous before installing

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ fi
 FRONTENDBASE="/usr/local/cpanel/base/frontend/$THEMENAME/pagecarton/"
 
 #   remove previous
-
 rm -rf pagecarton-cpanel-plugin
 rm -rf "$FRONTENDBASE"
 
@@ -18,6 +17,9 @@ git clone https://github.com/pagecarton/pagecarton-cpanel-plugin.git
 
 #   copy frontend
 cp -r pagecarton-cpanel-plugin/frontend/* "$FRONTENDBASE"
+
+#   uninstall
+/usr/local/cpanel/scripts/uninstall_plugin pagecarton-cpanel-plugin/configuration --theme="$THEMENAME"
 
 /usr/local/cpanel/scripts/install_plugin pagecarton-cpanel-plugin/configuration --theme "$THEMENAME" 
 
