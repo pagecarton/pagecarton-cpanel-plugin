049d44d90ad81ec16a86fe066697086aeeb03d44
Ayoola FALOLA <ayoola@ayoo.la>
2019-03-05 21:42:26 +0100

    Delete older hooks

--- new-account-auto-install.sh
+++ new-account-auto-install.sh
@@ -10,5 +10,8 @@ chmod 755 $INSTALL_FILENAME
 #	RUN THE INSTALLER
 #   $INSTALL_FILENAME
 
+# older files plugin
+/usr/local/cpanel/bin/manage_hooks delete script /usr/local/cpanel/3rdparty/bin/pagecarton/new-account-auto-install.php
+
 /usr/local/cpanel/bin/manage_hooks delete script $INSTALL_FILENAME
 /usr/local/cpanel/bin/manage_hooks add script $INSTALL_FILENAME
